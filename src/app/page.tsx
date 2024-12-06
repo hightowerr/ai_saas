f72da8320aece195a9b2c678f859c3f90acbb4e3
Olayinka Ola <69726083+hightowerr@users.noreply.github.com>
2024-12-06 10:46:38 +0000

    feat: Add new sections to the home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,22 @@
 'use client'
 
-import { HeroSection } from '@/components/HeroSection'
-import { FeaturesSection } from '@/components/FeaturesSection'
-import { PricingSection } from '@/components/PricingSection'
+import { HeroSection } from '../components/HeroSection'
+import { HowItWorksSection } from '../components/HowItWorksSection'
+import { FeaturesSection } from '../components/FeaturesSection'
+import { PricingSection } from '../components/PricingSection'
+import { FAQsSection } from '../components/FAQsSection'
+import { DemoSection } from '../components/DemoSection'
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-white">
-      <div className="mx-auto">
+      <div className="max-w-7xl mx-auto">
         <HeroSection />
+        <HowItWorksSection />
         <FeaturesSection />
         <PricingSection />
+        <FAQsSection />
+        <DemoSection />
       </div>
     </div>
   )
